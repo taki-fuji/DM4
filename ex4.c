109ddd8d5d442195dcc323217800a77c0466d04e
taki-fuji <86036185+taki-fuji@users.noreply.github.com>
2021-07-08 23:10:42 +0900

    判定パートを足した。

--- ex4.c
+++ ex4.c
@@ -13,8 +13,13 @@ printf("Hello, %s!\n",name);
     printf("Rolling the dice...\n");
     printf("Die 1:%d\n",d1);
     printf("Die 2:%d\n",d2);
-    printf("Total value: %d\n",d1+d2);    
-
+    printf("Total value: %d\n",d1+d2); 
+    //判定パート
+    if(d1+d2>=7){
+    printf("%s won!",name);
+}else{
+    printf("%s lost!",name);
+}
     return 0;
 
 }
