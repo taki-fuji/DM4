61e1b12a8d916dff22fc464222edfa086647ea4c
taki-fuji <86036185+taki-fuji@users.noreply.github.com>
2021-07-08 20:32:48 +0900

    最初のバージョン

---
+++ ex4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int i, num, heads=0, tails=0;
+
+printf("Tossing a coin...\n");
+
+for(i=0;i<3;i++){
+num=rand();
+if(num%2==0){
+printf("Round %d: Head\n,i+1");
+heads++;
+}else{
+printf("Round %d : Tail\n,i+1");
+tails++;
+}
+}
+printf("Heads: 5d, Tails: %d\n",heads,tails);
+return 0;
+
+}
