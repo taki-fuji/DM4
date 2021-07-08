d721221a75f1b0c4d0c6b1be4f9b1cd7028afc91
taki-fuji <86036185+taki-fuji@users.noreply.github.com>
2021-07-08 20:55:26 +0900

    名前を聞くパートを足した。(mainから)

--- ex4.c
+++ ex4.c
@@ -4,6 +4,13 @@
 int main(){
 int i, num, heads=0, tails=0;
 
+ //名前を聞くパートの編集(mainから)
+  char str[8];
+  printf("あんた誰？ ");
+  scanf("%s",str);
+  printf("ハロー、 %s\n",str);
+  
+  //コイントスのパート
 printf("Tossing a coin...\n");
 
 for(i=0;i<3;i++){
